de622aac44e9cec4ab91040b4d94aff8e5c41e56
MotherOfCat00 <asiakotwica245@gmail.com>
2022-11-21 11:19:52 +0100

    First 21.11.2022

--- app/src/main/java/edu/ib/hellobaby/MainActivity.kt
+++ app/src/main/java/edu/ib/hellobaby/MainActivity.kt
@@ -1,11 +1,26 @@
 package edu.ib.hellobaby
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        super.onTouchEvent(event)
+
+        when (event.action) {
+            MotionEvent.ACTION_UP -> {
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return true
     }
 }
