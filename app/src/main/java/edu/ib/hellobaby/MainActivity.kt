f4cb65243d4967a0893e144b017d131dcac086c1
MotherOfCat00 <asiakotwica245@gmail.com>
2022-11-26 18:38:31 +0100

    Fragment z kalendarzem - nie dziala

--- app/src/main/java/edu/ib/hellobaby/MainActivity.kt
+++ app/src/main/java/edu/ib/hellobaby/MainActivity.kt
@@ -1,26 +1,61 @@
 package edu.ib.hellobaby
 
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
-import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
+        readPermission()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        super.onTouchEvent(event)
+    //Function for asking for permission to write and read Google Calendar
+    public fun readPermission() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_CALENDAR
+        ) == PackageManager.PERMISSION_GRANTED
 
-        when (event.action) {
-            MotionEvent.ACTION_UP -> {
-                val intent = Intent(this, SignInActivity::class.java)
-                startActivity(intent)
+    public fun writePermission() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.WRITE_CALENDAR
+        ) == PackageManager.PERMISSION_GRANTED
+
+    public fun requestPermissions() {
+        var permissionsToRequest = mutableListOf<String>()
+        if (!readPermission()) {
+            permissionsToRequest.add(Manifest.permission.READ_CALENDAR)
+        }
+        if (!writePermission()) {
+            permissionsToRequest.add(Manifest.permission.WRITE_CALENDAR)
+        }
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 0)
+        }
+    }
+
+    //Function for asking for permission to write and read Google Calendar
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0 && grantResults.isNotEmpty()) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    // Log.d("PermisionRequest", "${permissions[i]} granted")
+                }
             }
         }
-        return true
     }
+
 }
--- app/src/main/java/edu/ib/hellobaby/calFragment.kt
+++ app/src/main/java/edu/ib/hellobaby/calFragment.kt
@@ -1,18 +1,61 @@
 package edu.ib.hellobaby
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import kotlinx.android.synthetic.main.fragment_cal.*
+import kotlinx.android.synthetic.main.fragment_cal.view.*
+import java.util.*
 
-class calFragment : Fragment() {
+
+class calFragment : Fragment(R.layout.fragment_cal) {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cal, container, false)
-    }
 
+        val view: View = inflater.inflate(R.layout.fragment_cal, container, false)
+
+        getDate.setOnDateChangeListener { calView: CalendarView, year: Int, month: Int, dayOfMonth: Int ->
+
+            // Create calender object with which will have system date time.
+            val calender: Calendar = Calendar.getInstance()
+
+            // Set attributes in calender object as per selected date.
+            calender.set(year, month, dayOfMonth)
+
+            // Now set calenderView with this calender object to highlight selected date on UI.
+            calView.setDate(calender.timeInMillis, true, true)
+            // Log.d("SelectedDate", "$dayOfMonth/${month + 1}/$year")
+
+        }
+
+        view.calcDatesButton.setOnClickListener { view ->
+
+            val example = "xdddd"
+
+            // Fetch long milliseconds from calenderView.
+            val dateMillis: Long = getDate.date
+
+            // Create Date object from milliseconds.
+            val date = Date(dateMillis)
+
+            val selectedDay = DateFormat.format("dd", date).toString().toInt()
+            var selectedMonthNumber = DateFormat.format("MM", date).toString().toInt()
+            selectedMonthNumber -= 1
+            val selectedYear = DateFormat.format("yyyy", date).toString().toInt()
+
+            // Log.d("SelectedDate", "$selectedDay oraz ${selectedMonthNumber + 1} oraz $selectedYear")
+            print(example)
+
+            testText.text = "nothing"
+
+        }
+        return view
+    }
 }
