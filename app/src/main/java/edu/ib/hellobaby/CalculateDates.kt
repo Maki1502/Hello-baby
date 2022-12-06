898b231ed54aae7b2fcee64e3f794ed8c7264ea4
MotherOfCat00 <asiakotwica245@gmail.com>
2022-12-06 10:05:14 +0100

    Działające obliczenia daty porodu, baby shower i gender reveal.

--- app/src/main/java/edu/ib/hellobaby/CalculateDates.kt
+++ app/src/main/java/edu/ib/hellobaby/CalculateDates.kt
@@ -3,13 +3,21 @@ package edu.ib.hellobaby
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
+import android.icu.text.MessageFormat.format
 import android.os.Bundle
+import android.text.format.DateFormat
+import android.text.format.DateFormat.format
 import android.util.Log
 import android.widget.Adapter
 import android.widget.CalendarView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_calculate_dates.*
+import java.lang.String.format
+
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.Period
 import java.util.*
 
 
@@ -25,8 +33,6 @@ class CalculateDates : AppCompatActivity() {
         val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,
             Context.MODE_PRIVATE)
 
-        setupViewPager2()
-
         // Function to get actual date from system calendar
         getDate.setOnDateChangeListener { calView: CalendarView, year: Int, month: Int, dayOfMonth: Int ->
             // Create calender object with which will have system date time.
@@ -35,6 +41,7 @@ class CalculateDates : AppCompatActivity() {
             calender.set(year, month, dayOfMonth)
             // Now set calenderView with this calender object to highlight selected date on UI.
             calView.setDate(calender.timeInMillis, true, true)
+            Log.d("data systemowa", "$calender")
         }
 
         // Function to get date from calendar
@@ -45,34 +52,30 @@ class CalculateDates : AppCompatActivity() {
             val date = Date(dateMillis)
             Log.d("data", "$date")
 
- /*           val selectedDay = DateFormat.format("dd", date).toString().toInt()
+            var selectedDay = DateFormat.format("dd", date).toString().toInt()
             var selectedMonthNumber = DateFormat.format("MM", date).toString().toInt()
-            selectedMonthNumber -= 1
-            val selectedYear = DateFormat.format("yyyy", date).toString().toInt()*/
+            val selectedYear = DateFormat.format("yyyy", date).toString().toInt()
+            var localDate = LocalDate.of(selectedYear, selectedMonthNumber, selectedDay)
 
             // Save period date
             val editor:SharedPreferences.Editor =  sharedPreferences.edit()
             editor.putLong("periodDate", dateMillis)
 
+            var pregnacy = Period.of(0, 0, 270)
+            var birthDate = localDate.plus(pregnacy)
+            Log.d("mod date", "$birthDate")
+
+            //gender reveal po USG  ok 20 tyg
+            val genderLength = Period.of(0,0,140)
+            val genderReveal = localDate.plus(genderLength)
+            Log.d("gender", "$genderReveal")
+
+            // baby shower
+            val showerLength = Period.of(0,0,210)
+            val babyShower = localDate.plus(showerLength)
+            Log.d("baby", "$babyShower")
         }
 
-    }
-
-    private fun setupViewPager2() {
-        val list: MutableList<String> = ArrayList()
-        list.add("This is your First Screen")
-        list.add("This is your Second Screen")
-        list.add("This is your Third Screen")
-        list.add("This is your Fourth Screen")
-
-        val colorList: MutableList<String> = ArrayList()
-        colorList.add("#00ff00")
-        colorList.add("#ff0000")
-        colorList.add("#0000ff")
-        colorList.add("#f0f0f0")
-
-        // Set adapter to viewPager.
-        datesPager.adapter = PagerAdapter(this, list, colorList)
     }
 
 }
